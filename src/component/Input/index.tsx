78b679e96ef7a294ff73dfcf3f543afb30485e2f
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2021-02-17 16:08:16 +0200

    add formik

--- src/component/Input/index.tsx
+++ src/component/Input/index.tsx
@@ -1,10 +1,26 @@
-import React ,{FC} from 'react';
-import TextFiled from '../../common/TextField'
+import React ,{FC, useState} from 'react';
+import Box from '../../common/Box';
+import TextField from '../../common/TextField'
+
+interface InputProps {
+    input:string | number | null
+}
+
+
 
 const InputComponent:FC =() =>{
+
     return(
         <>
-        <TextFiled>  </TextFiled>
+        <form>
+        <Box pt={1} pl={1} display="flex" flexDirection="column">
+            <TextField label="Full Name" id="standard-size-small" defaultValue="Placeholder" size="small" />
+
+            <TextField label="Active" id="standard-size-small" defaultValue="Placeholder" size="small" >  </TextField>
+            <TextField error id="standard-error-helper-text" label="Error" placeholder="Placeholder" helperText="Incorrect entry."
+        />
+            </Box>
+        </form>
         </>
     );
 } 
